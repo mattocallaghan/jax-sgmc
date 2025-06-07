50a30eab73ea61b8cf01ddb48a68de19311afe61
Matthew O'Callaghan <33609307+mattocallaghan@users.noreply.github.com>
2025-06-07 09:58:20 +0100

    Update io.py

--- jax_sgmc/io.py
+++ jax_sgmc/io.py
@@ -700,7 +700,7 @@ def save(data_collector: DataCollector = None,
     # flattening on the host.
     chain_id, data = args
     flat_args = (chain_id, tree_util.tree_leaves(data))
-    counter = counter = io_callback(_save,(), arg)
+    counter = io_callback(_save,(), flat_args)
     return counter
 
   def init(init_sample, init_checkpoint, static_information) -> saving_state:
