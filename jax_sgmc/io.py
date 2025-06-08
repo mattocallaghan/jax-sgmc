8b41320a3b46bec4cc656e2fc503acc05336f0b4
Matthew O'Callaghan <33609307+mattocallaghan@users.noreply.github.com>
2025-06-08 15:21:54 +0100

    Update io.py

--- jax_sgmc/io.py
+++ jax_sgmc/io.py
@@ -761,8 +761,7 @@ def save(data_collector: DataCollector = None,
   def postprocess(state: saving_state, unused_saved):
     # Call with host callback to ensure that finalized is called after all other
     # id_tap processes were finished.
-    host_callback.id_tap(
-      lambda id, *unused: data_collector.finalize(id),
+   io_callback(lambda id, *unused: data_collector.finalize(id),None,
       state.chain_id)
     collected_samples = data_collector.finished(state.chain_id)
     return {"sample_count": state.saved_samples,
